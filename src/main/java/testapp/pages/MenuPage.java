944e9dd56c79e7dc68ee5b0661e225949ec5b642
Denis <malzoff2003@gmail.com>
2018-12-03 02:03:12 +0700

    DUELS-2 Главная страница

--- src/main/java/testapp/pages/MenuPage.java
+++ src/main/java/testapp/pages/MenuPage.java
@@ -3,8 +3,10 @@ package testapp.pages;
 import org.apache.wicket.MarkupContainer;
 import org.apache.wicket.markup.html.link.StatelessLink;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
+import testapp.WebSession;
 
 import static testapp.game.PlayerState.IDLE;
+import static testapp.game.PlayerState.IN_QUEUE;
 
 public class MenuPage extends BasePage {
 
@@ -13,13 +15,13 @@ public class MenuPage extends BasePage {
     public MenuPage(PageParameters parameters) {
         super(parameters);
 
+        setPlayerState(IDLE);
         add(new StatelessLink<MarkupContainer>("enterQueue") {
             @Override
             public void onClick() {
                 if (getPlayerState() == IDLE) {
+                    setPlayerState(IN_QUEUE);
                     setResponsePage(QueuePage.class);
-                } else {
-                    setPlayerState(IDLE);
                 }
             }
         });
@@ -27,7 +29,8 @@ public class MenuPage extends BasePage {
         add(new StatelessLink<MarkupContainer>("Exit") {
             @Override
             public void onClick() {
-
+                WebSession.get().logout();
+                setResponsePage(HomePage.class, new PageParameters());
             }
         });
     }
